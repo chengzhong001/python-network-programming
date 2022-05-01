267ec57dc669cea14b94d460f2a2ebafc594de6a
azhong <zczwzw@163.com>
2022-05-01 19:27:49 +0800

    tcp server client

---
+++ 01-Sockets-IPV4-Client-Server/13a_echo_server.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import argparse
+import socket
+
+
+host = "localhost"
+data_playload = 2048
+backlog = 5
+
+
+def echo_server(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    server_address = (host, port)
+    sock.bind(server_address)
+    sock.listen(backlog)
+    while True:
+        print("Waiting to receive message from client")
+        client, address = sock.accept()
+        data = client.recv(data_playload)
+        if data:
+            print(f"Data: {data.decode()}")
+            client.send(data)
+            print(f"sent {data.decode()} bytes back to {address}")
+            client.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Socket Server Example")
+    parser.add_argument("--port", action="store", dest="port", type=int, required=True)
+    given_args = parser.parse_args()
+    port = given_args.port
+    echo_server(port)
---
+++ 01-Sockets-IPV4-Client-Server/13b_echo_client.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import socket
+import argparse
+
+host = "localhost"
+
+
+def echo_client(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = (host, port)
+    print(f"Connectiong to {port} {server_address}")
+    sock.connect(server_address)
+
+    try:
+        message = "Test message. This will be echoed".encode()
+        print(f"Sending {message}")
+        sock.sendall(message)
+        amount_received = 0
+        amount_expected = len(message)
+        while amount_received < amount_expected:
+            data = sock.recv(16)
+            amount_received += len(data)
+            print(f"Received: {data.decode()}")
+
+    except socket.errno as e:
+        print(f"Socket error: {e}")
+    except Exception as e:
+        print(f"Other exception: {e}")
+    finally:
+        print("Closing connection to the server")
+        sock.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Socket Server Examle")
+    parser.add_argument("--port", action="store", dest="port", type=int, required=True)
+    given_args = parser.parse_args()
+    port = given_args.port
+    echo_client(port)
