e41ef7276f4f2e65029cffaead57acca8cab76be
azhong <zczwzw@163.com>
2022-09-25 21:23:05 +0800

    finshi

--- 03-IPv6-Unix-Socket/3_2_ping_remote_host.py
+++ 03-IPv6-Unix-Socket/3_2_ping_remote_host.py
@@ -18,21 +18,22 @@ class Pinger:
 
     def do_checksum(self, source_string):
         sum = 0
-        max_count = (len(source_string) // 2) * 2
+        max_count = (len(source_string) / 2) * 2
         count = 0
         while count < max_count:
-            val = ord(source_string[count + 1] * 256 + ord(source_string[count]))
-            sum = sum + val
-            sum = sum & 0xFFFFFFFF
-            count = count + 2
+            val = source_string[count + 1] * 256 + source_string[count]
+            sum += val
+            sum &= 0xFFFFFFFF
+            count += 2
 
         if max_count < len(source_string):
-            sum = sum + ord(source_string)
-            sum = sum & 0xFFFFFFFF
+            sum += ord(source_string[len(source_string) - 1])
+            sum &= 0xFFFFFFFF
 
         sum = (sum >> 16) + (sum & 0xFFFF)
-        sum = sum + (sum >> 16)
-        answer = answer & 0xFFFF
+        sum +=  (sum >> 16)
+        answer = ~sum
+        answer &= 0xFFFF
         answer = answer >> 8 | (answer << 8 & 0xFF00)
         return answer
 
@@ -62,4 +63,59 @@ class Pinger:
             if time_remaining <= 0:
                 return
 
-    
+    def send_ping(self, sock: socket.socket, ID):
+        target_addr = socket.gethostbyname(self.target_host)
+        my_checksum = 0
+        header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, my_checksum, ID, 1)
+        bytes_in_double = struct.calcsize("d")
+        data = (192 - bytes_in_double) * "Q"
+        data = struct.pack("d", time.time()) + bytes(data.encode("utf-8"))
+        my_checksum = self.do_checksum(header + data)
+        header = struct.pack(
+            "bbHHh", ICMP_ECHO_REQUEST, 0, socket.htons(my_checksum), ID, 1
+        )
+        packet = header + data
+        sock.sendto(packet, (target_addr, 1))
+
+    def ping_once(self):
+        icmp = socket.getprotobyname("icmp")
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
+
+        except socket.error as e:
+            if e.errno == 1:
+                e.msg = "ICMP messages can only be sent from root user processes"
+                raise socket.error(e.msg)
+        except Exception as e:
+            print("Exception:", e)
+
+        my_ID = os.getpid() & 0xFFFF
+        self.send_ping(sock, my_ID)
+        delay = self.receive_pong(sock, my_ID, self.timeout)
+        sock.close
+        return delay
+
+    def ping(self):
+        for i in range(self.count):
+            print(f"Ping to {self.target_host}...")
+            try:
+                delay = self.ping_once()
+            except socket.gaierror as e:
+                print(f"Ping failed. (socket error: {e[1]})")
+
+            if delay == None:
+                print(f"Ping failed. (timeout within {self.timeout}sec.)")
+            else:
+                delay = delay * 1000
+                print("Got pong in {:.4f} ms".format(delay))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Python ping")
+    parser.add_argument(
+        "--target-host", action="store", dest="target_host", required=True
+    )
+    given_args = parser.parse_args()
+    target_host = given_args.target_host
+    pinger = Pinger(target_host=target_host)
+    pinger.ping()
